0cb66c86b782d451bf00ca1b4984196207cfa977
Dj4rn0 <d.eijkenboom@sintlucasedu.nl>
2023-06-08 14:20:40 +0200

    Created Idle PathFinding

--- Assets/Scripts/Djarno Scripts/CustomEnemyIdleMovement.cs
+++ Assets/Scripts/Djarno Scripts/CustomEnemyIdleMovement.cs
@@ -3,28 +3,50 @@ using System.Collections;
 
 namespace Pathfinding
 {
-	[UniqueComponent(tag = "ai.destination")]
-	[HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_a_i_destination_setter.php")]
-	public class AIDestinationSetter : VersionedMonoBehaviour
-	{
-		public Transform target;
-		IAstarAI ai;
+    /// <summary>
+    /// Sets the destination of an AI to the position of a specified object.
+    /// This component should be attached to a GameObject together with a movement script such as AIPath, RichAI, or AILerp.
+    /// This component will then make the AI move towards the <see cref="target"/> set on this component.
+    ///
+    /// See: <see cref="Pathfinding.IAstarAI.destination"/>
+    ///
+    /// [Open online documentation to see images]
+    /// </summary>
+    [UniqueComponent(tag = "ai.destination")]
+    [HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_a_i_destination_setter.php")]
+    public class CustomEnemyIdleMovement : VersionedMonoBehaviour
+    {
+        /// <summary>The object that the AI should move to</summary>
+        public Transform target;
+        public Transform targetB;
+        private AIPath aiPath;
+        private bool reachedTarget = false;
 
-		void OnEnable()
-		{
-			ai = GetComponent<IAstarAI>();
-			if (ai != null) ai.onSearchPath += Update;
-        }
-
-        void OnDisable()
+        private void Start()
         {
-            if (ai != null) ai.onSearchPath -= Update;
+            aiPath = GetComponent<AIPath>();
         }
 
-        void Update()
-		{
-			if (target != null && ai != null) ai.destination = target.position;
-		}
-	}
-}
+        private void Update()
+        {
+            if (!reachedTarget)
+            {
+                SetDestination(target);
+            }
+            else
+            {
+                SetDestination(targetB);
+            }
+        }
 
+        private void SetDestination(Transform destination)
+        {
+            aiPath.destination = destination.position;
+            float distanceToTarget = Vector3.Distance(transform.position, destination.position);
+            if (distanceToTarget <= aiPath.endReachedDistance)
+            {
+                reachedTarget = !reachedTarget;
+            }
+        }
+    }
+}
