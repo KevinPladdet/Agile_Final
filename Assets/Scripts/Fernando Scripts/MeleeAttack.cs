c82a8e7adee6983764015db9e7421d31beae6bb5
Fernando <F.kok@sintlucasedu.nl>
2023-06-15 11:33:39 +0200

    push

--- Assets/Scripts/Fernando Scripts/MeleeAttack.cs
+++ Assets/Scripts/Fernando Scripts/MeleeAttack.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class MeleeAttack : MonoBehaviour
 {
-    public List<GameObject> Enemies;
+    //public List<GameObject> Enemies;
     [Header("Backstab")]
     public bool CanBackStab = false;
     
@@ -22,7 +22,11 @@ public class MeleeAttack : MonoBehaviour
 
     bool canAttack = true;
 
-    public List<GameObject> Enemies;
+    private enum AttackType { None, Small, Small2, Big, Backstab }
+
+    private AttackType currentAttack = AttackType.None;
+
+    //public List<GameObject> Enemies;
 
     private void Start()
     {
@@ -36,25 +40,25 @@ public class MeleeAttack : MonoBehaviour
         {
             comboStep = 2;
             Debug.Log("1");
-            StartCoroutine(PerformComboStep(0.5f, SmallAttack1));
+            StartCoroutine(PerformComboStep(0.5f, SmallAttack1, AttackType.Small));
         }
         else if (Input.GetKeyDown(KeyCode.Mouse0) && onEnemy && comboStep == 2 && canAttack == true)
         {
             comboStep = 3;
             Debug.Log("2");
-            StartCoroutine(PerformComboStep(0.5f, SmallAttack2));
+            StartCoroutine(PerformComboStep(0.5f, SmallAttack2, AttackType.Small2));
         }
         else if (Input.GetKeyDown(KeyCode.Mouse0) && onEnemy && comboStep == 3 && canAttack == true)
         {
             comboStep = 1;
             Debug.Log("3");
-            StartCoroutine(PerformComboStep(1f, BigAttack));
+            StartCoroutine(PerformComboStep(1f, BigAttack, AttackType.Big));
         }
         if (Input.GetKeyDown(KeyCode.F) && onEnemy && comboStep == 1 && canAttack == true && CanBackStab == true)
         {
             comboStep = 1;
             Debug.Log("BackStab");
-            StartCoroutine(PerformComboStep(1f, BackStab));
+            StartCoroutine(PerformComboStep(1f, BackStab, AttackType.Backstab));
         }
         // ComboSystem
         if (comboStep > 1)
@@ -68,29 +72,52 @@ public class MeleeAttack : MonoBehaviour
         }
     }
 
-    private IEnumerator PerformComboStep(float delay, System.Action attackAction)
+    private IEnumerator PerformComboStep(float delay, System.Action attackAction, AttackType preformedAttack)
     {
+        currentAttack = preformedAttack;
         canAttack = false;
         yield return new WaitForSeconds(delay);
-        attackAction.Invoke();
+        //attackAction.Invoke();
         comboTimer = 1.5f;
         canAttack = true;
+        currentAttack = AttackType.None;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if enemy is in range
-        if (other.gameObject == enemy)
+        if (other.gameObject.CompareTag("Enemy"))
         {
             onEnemy = true;
             direction = other.transform.position - transform.position;
+            Debug.Log(currentAttack);
+            switch (currentAttack)
+            {
+                case AttackType.None:
+                    break;
+                case AttackType.Small:
+            Debug.Log("Hit");
+                    other.gameObject.GetComponent<Damage>().TakeDamage();
+                    break;
+                case AttackType.Small2:
+                    other.gameObject.GetComponent<Damage>().TakeDamage();
+                    break;
+                case AttackType.Big:
+                    other.gameObject.GetComponent<Damage>().TakeDamage2();
+                    break;
+                case AttackType.Backstab:
+                    other.gameObject.GetComponent<Damage>().BackStab();
+                    break;
+                default:
+                    break;
+            }
         }
+        
     }
 
     private void OnTriggerExit(Collider other)
     {
         // Check if enemy is out of range
-        if (other.gameObject == enemy)
+        if (other.gameObject.CompareTag("Enemy"))
         {
             onEnemy = false;
         }
